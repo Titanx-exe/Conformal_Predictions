e28327e0403a6bca875be95fa0729e5a43c07a5b
arnabsharma91 <arnabsh91@gmail.com>
2025-01-01 14:08:03 +0100

    Change noise parameter

--- optimizers/noise.py
+++ optimizers/noise.py
@@ -8,7 +8,7 @@ print(ds)
 '''
 prev_noise=None
 
-def add_gausian_noise(optimizer,device,mu=0.0,sigma=50.0):
+def add_gausian_noise(optimizer,device,mu=10.0,sigma=50.0):
     for group in optimizer.param_groups:
         for p in group['params']:
             if p.requires_grad:
