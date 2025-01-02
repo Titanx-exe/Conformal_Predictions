40ddd00f64711b8e0813b400b4cb720e1543c02f
arnabsharma91 <arnabsh91@gmail.com>
2025-01-02 22:35:00 +0100

    added different recalls

--- Evaluator.py
+++ Evaluator.py
@@ -178,7 +178,7 @@ class IndexEvaluator:
         self.params = params
         self.documents = list(self.documents)
         self.entities = list(self.entities.keys())
-    '''
+   '''
     def generate_random_samples(self):
         samples=[]
         for doc in self.documents:
@@ -191,48 +191,49 @@ class IndexEvaluator:
                         candidates.append(cnd)
                 samples.append((doc,candidates))
         return samples
-    def evaluate(self,model,random_samples=True,k=10):
-        index = indexing.index_entities(model,self.entities, self.collator)
 
-        data_loader=DataLoader(self.documents, shuffle=False, batch_size=100,
-                                  collate_fn=self.collator.collate_context)
+    def evaluate(self, model, random_samples=True, max_k=100):
+        index = indexing.index_entities(model, self.entities, self.collator)
+
+        data_loader = DataLoader(self.documents, shuffle=False, batch_size=100,
+                                 collate_fn=self.collator.collate_context)
         if self.params["silent"]:
             iter_ = data_loader
         else:
             iter_ = tqdm(data_loader, desc="Encode Eval Queries")
-        doc_encodings=[]
+
+        doc_encodings = []
         for step, batch in enumerate(iter_):
-            if not isinstance(model,E5Ranker):
-
+            if not isinstance(model, E5Ranker):
                 context_input = batch["context_input"]
-                #candidate_input = batch["candidate_input"]
-                #labels=[0 for i in range(batch["candidate_input"].size(0))]
-                #label_input = batch[0]["label_idx"].to(device)
-                # label_input = torch.LongTensor(torch.zeros(candidate_input.size(0),dtype=torch.int64)).to(device)
-                # context_input, candidate_input, label_input = batch
-                encodings=model.encode_context(context_input).tolist()
+                encodings = model.encode_context(context_input).tolist()
             else:
-                encodings=model.encode_context(batch)
+                encodings = model.encode_context(batch)
             doc_encodings.extend(encodings)
-        found_ents=index.search(doc_encodings,k)
-        all_labels=[]
-        all_scores=[]
-        indexes=[]
-        all_found=0
-        all_not_found=0
-        for i in range(len(self.documents)):
-            correct_entities=self.doc_to_ent[self.documents[i]]
-            prediction=found_ents[i]
-            for en in correct_entities:
-                if en in prediction:
-                    all_found+=1
-                else:
-                    all_not_found+=1
-        print("found:"+str(all_found)+" not found:"+str(all_not_found))
-        results=all_found/(all_not_found+all_found)
-        #print(result)
-        return index,results
 
+        found_ents = index.search(doc_encodings, max_k)
+        recalls = {5: 0, 10: 0, 30: 0, 100: 0}
+        total_correct = 0
+
+        for i in range(len(self.documents)):
+            correct_entities = set(self.doc_to_ent[self.documents[i]])
+            prediction = found_ents[i]
+
+            for k in recalls.keys():
+                if any(entity in correct_entities for entity in prediction[:k]):
+                    recalls[k] += 1
+            total_correct += len(correct_entities)
+
+        for k, count in recalls.items():
+            recalls[k] = count / len(self.documents)
+
+        print("Recall@5: {:.5f}".format(recalls[5]))
+        print("Recall@10: {:.5f}".format(recalls[10]))
+        print("Recall@30: {:.5f}".format(recalls[30]))
+        print("Recall@100: {:.5f}".format(recalls[100]))
+
+        return index, recalls
+    
     def evaluate_mrr(self, model, random_samples=True, k=10):
         index = indexing.index_entities(model, self.entities, self.collator)
 
