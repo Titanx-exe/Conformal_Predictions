a8d2cc3cc8db80e0882cc394153a70f8d32f3454
arnabsharma91 <arnabsh91@gmail.com>
2025-01-08 08:41:19 +0100

    added different recalls

--- optimizers/noise.py
+++ optimizers/noise.py
@@ -60,7 +60,7 @@ prev_noise = add_anticorrelated_noise_method2_dynamic_alpha(optimizer, device, a
 '''
 
 
-def add_anticorrelated_noise_gradient(optimizer, device, beta_0=0.5, sigma=1.0):
+def add_anticorrelated_noise_gradient(optimizer, device, beta_0=0.1, sigma=1.0):
     """Applies gradient-based anticorrelated noise with dynamic beta."""
 
     for group in optimizer.param_groups:
